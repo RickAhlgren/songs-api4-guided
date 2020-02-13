9d20677d3e066bf4fba9e789de4f515bcdc812c7
thericktastic <love@The-RT-MBP.attlocal.net>
2020-02-13 13:30:07 -0500

    Make port dynamic

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ server.get("/", (request, response) => {
     response.status(200).json(songs)
 })
 
-const port = 5000;
+// Make the port dynamic so it can run on Heroku AND my local machine
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`\n*** Running on port ${port} ***\n`)
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Deployment Notes
+
+## Steps
+
+- create an empty repo on Github
+- clone it
+- open it in code editor
+- create a default package.json
+- npm i express
+- `git status` => `node_modules`
+- `npx gitignore node` to generate a `.gitignore` super file
